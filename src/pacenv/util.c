b7cac44c220a56c10af2e80e4dbd9f744008f53d
beryll1um <vitalii.babichuk@proton.me>
2025-07-10 01:01:08 +0200

    Fix bad naming practices
    I already said that I am a bit silly and very picky about details:
    - Names were fixed.
    - Config unit was removed, as it is completely unnecessary here.
    - Error messages were predefined by a macro.

--- src/pacenv/util.c
+++ src/pacenv/util.c
@@ -6,13 +6,53 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <fcntl.h>
 #include <string.h>
 #include <limits.h>
+#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <pacenv/glob.h>
+#include <json-c/json.h>
 
-static int mkdir_recurs(const char* path, char* stage, mode_t mode)
+// You may disapprove this little shortcut. It makes me feel better.
+#define FAILED_TO_ACTION_FILE_STR(ACTION) "failed to " ACTION " file"
+
+struct json_object* pacenv_jso_parse(const char* filepath)
+{
+	int fd = open(filepath, O_RDONLY);
+	if (fd == -1)
+	{
+		fprintf(stderr, "%s: " FAILED_TO_ACTION_FILE_STR("open") ": %s: %s\n",
+			g_filename, filepath, strerror(errno));
+		return NULL;
+	}
+	struct json_object* jso = json_object_from_fd(fd);
+	if (close(fd) == -1)
+	{
+		fprintf(stderr, "%s: " FAILED_TO_ACTION_FILE_STR("close") ": %s: %s\n",
+			g_filename, filepath, strerror(errno));
+		// This is a problem, but there is no need to interrupt execution.
+	}
+	if (!jso)
+	{
+		// Thanks JSON-C developers for the newline they are adding
+		// to the end of each error.
+		fprintf(stderr, "%s: " FAILED_TO_ACTION_FILE_STR("parse") ": %s: %s",
+			g_filename, filepath, json_util_get_last_err());
+		return NULL;
+	}
+	if (json_object_get_type(jso) != json_type_object)
+	{
+		fprintf(stderr, "%s: type mismatch of file content: %s: "
+			"must be object\n", g_filename, filepath);
+		json_object_put(jso);
+		return NULL;
+	}
+	return jso;
+}
+
+static int makedir(const char* path, char* stage, mode_t mode)
 {
 	char* slash = strchr(stage, '/');
 	if (slash)
@@ -28,18 +68,18 @@ static int mkdir_recurs(const char* path, char* stage, mode_t mode)
 	if (slash)
 	{
 		*slash = '/';
-		return mkdir_recurs(path, slash + 1, mode);
+		return makedir(path, slash + 1, mode);
 	}
 	return 0;
 }
 
-int mkdir_p(const char* path, mode_t mode)
+int pacenv_makedir(const char* path, mode_t mode)
 {
 	char buf[PATH_MAX];
 	size_t len = strnlen(path, sizeof(buf) - 1);
 	memcpy(buf, path, len);
 	buf[len] = '\0';
-	return mkdir_recurs((const char*)buf, buf, mode);
+	return makedir((const char*)buf, buf, mode);
 }
 
 // vim: set ts=4 sw=4 noexpandtab:
--- src/pacenv/util.h
+++ src/pacenv/util.h
@@ -4,7 +4,20 @@
 
 #include <sys/types.h>
 
+// Forward declaration of the JSON object from the json-c library.
+struct json_object;
+
+// I am not happy with the default json-c library from file parsing
+// as it isn't verbose enough.
+struct json_object* pacenv_jso_parse(const char* filepath);
+
+// I would like to unify this and avoid spelling problems in the future.
+#define PACENV_TYPE_MISMATCH_STR(WHAT, WHERE, EXPECT) \
+	"type mismatch for " WHAT " in " WHERE ": must be " EXPECT
+#define PACENV_MISSING_PROP_STR(WHAT, WHERE) \
+	"missing property " WHAT " in " WHERE
+
 // This is similar to 'mkdir -p', but looks much better!
-int mkdir_p(const char* path, mode_t mode);
+int pacenv_makedir(const char* path, mode_t mode);
 
 // vim: set ts=4 sw=4 noexpandtab:
